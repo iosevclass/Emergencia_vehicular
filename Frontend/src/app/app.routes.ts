ed384ccb470b0c05c1e8af523d73804d75ec0d6e
NowIose <dainerdual@gmail.com>
2026-04-24 20:00:45 -0400

    landing page creado y enlazado

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { HomeComponent } from './features/home/home.component';
+import { LandingComponent } from './features/home-page/landing.component';
 
 export const routes: Routes = [
   // Cambiamos el redireccionamiento para que apunte a 'login'
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent }, // Asegúrate de tener un HomeComponent creado
---
+++ Frontend/src/app/features/home-page/landing.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {
+  constructor(private router: Router) {}
+
+  // Redirigir al taller al login
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  // Redirigir al taller al registro
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+
+  // Aquí pondrás el link de descarga de la App más tarde
+  downloadApp() {
+    window.open('https://tu-url-de-descarga.com', '_blank');
+  }
+}
